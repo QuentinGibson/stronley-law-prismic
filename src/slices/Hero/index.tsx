7a32e708adebea6efc235c2632e2644cc3e94567
Quentin Gibson <quentingibson94@gmail.com>
2024-04-02 16:01:17 -0400

    add hero slice

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,7 +1,7 @@
-import { Content } from "@prismicio/client";
+import { Content, isFilled } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import Bounded from "@/app/components/Bounded";
-import { PrismicNextLink } from "@prismicio/next";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 
 /**
  * Props for `Hero`.
@@ -13,19 +13,47 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <Bounded
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      <PrismicRichText field={slice.primary.title} />
-      <PrismicRichText field={slice.primary.body} />
-      <PrismicNextLink field={slice.primary.primary_link}>
-        <>{slice.primary.primary_cta}</>
-      </PrismicNextLink>
-      <PrismicNextLink field={slice.primary.secondary_link}>
-        <>{slice.primary.secondary_cta}</>
-      </PrismicNextLink>
-    </Bounded>
+    <section className="text-white">
+      {isFilled.image(slice.primary.background_image) && (
+        <div className="absolute top-0 h-[800px] w-full">
+          <PrismicNextImage
+            field={slice.primary.background_image}
+            alt=""
+            fill
+            className="pointer-events-none -z-10 select-none object-cover"
+          />
+          <div className="absolute inset-0 -z-[9] bg-black opacity-50" />
+        </div>
+      )}
+      <Bounded
+        data-slice-type={slice.slice_type}
+        data-slice-variation={slice.variation}
+        as={"div"}
+      >
+        <div className="flex flex-col">
+          <h1 className="text-5xl font-bold uppercase md:text-7xl">
+            <PrismicRichText field={slice.primary.title} />
+          </h1>
+          <p className="max-w-xl text-balance">
+            <PrismicRichText field={slice.primary.body} />
+          </p>
+          <div className="flex gap-3">
+            <PrismicNextLink
+              className="bg-blue-500 px-5 py-2"
+              field={slice.primary.primary_link}
+            >
+              <>{slice.primary.primary_cta}</>
+            </PrismicNextLink>
+            <PrismicNextLink
+              className="bg-blue-700 px-5 py-2"
+              field={slice.primary.secondary_link}
+            >
+              <>{slice.primary.secondary_cta}</>
+            </PrismicNextLink>
+          </div>
+        </div>
+      </Bounded>
+    </section>
   );
 };
 
